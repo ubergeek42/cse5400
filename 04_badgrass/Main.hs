dc22f7751ce65f7ea70f252d44fe23ff8eacd89c
Keith Johnson <kj@ubergeek42.com>
2011-03-29 22:40:11 -0400

    Cleanup/Readability/Comments.(For real this time)

--- 04_badgrass/Main.hs
+++ 04_badgrass/Main.hs
@@ -8,28 +8,36 @@ module Main where
 
 import Text.Printf
 import Data.Array
-import Debug.Trace
 
 main :: IO()
 main = interact(display . map calculate . readInput)
 
--- Rows, Columns, Array representing the "map"
-type GrassMap = (Int, Int, Array (Int,Int) Int)
 
--- Make a list of integers, and pass that to getPairs
-readInput :: String -> [GrassMap]
+-- A 2D array that holds Ints
+type GrassMap = Array (Int,Int) Int
+-- Width, Height, Array representing the grass
+type Pasture = (Int, Int, GrassMap)
+
+-- Make a list of integers, and pass that to parseNumbers
+readInput :: String -> [Pasture]
 readInput = parseNumbers . map read . words
 
--- (Rows, Columns) [Entire input]
-parseNumbers :: [Int] -> [GrassMap]
+{- Takes 2 numbers from the front, calls them the width/height, and creates an
+ - array from the input of that size.  It then recursively calls itself until
+ - there is no more input left.
+ -}
+parseNumbers :: [Int] -> [Pasture]
 parseNumbers [] = []
 parseNumbers (h:w:xs) = (w,h,gmap) : parseNumbers rest
     where
       (gmap,rest) = getArray w h xs
 
--- Builds an array from the input
-getArray:: Int->Int->[Int]->(Array (Int,Int) Int, [Int])
---getArray _ _ [] = (array (0,0), [])
+{- Takes in the width, height, and the rest of the input
+ - Returns a pair containing:
+   * a new 2D Data.Array of the specified width/height
+   * Any input that was unused
+ -}
+getArray:: Int->Int->[Int]->(GrassMap, [Int])
 getArray 0 _ _ = error "Invalid width(Must be greater than zero)"
 getArray _ 0 _ = error "Invalid height(Must be greater than zero)"
 getArray w h xs = (array ((0,0),(h-1,w-1)) (zip ix mdata) , rest)
@@ -38,24 +46,38 @@ getArray w h xs = (array ((0,0),(h-1,w-1)) (zip ix mdata) , rest)
           mdata = take (w*h) xs
           rest = drop (w*h) xs
 
--- Given a n x m grid, each path must be n+m in length, and each path must contain n Ups, and m Rights
--- This basically boils down to m+n choose m, or m+n choose n, which is effectively (n+m)!/(n!m!)
-calculate :: GrassMap -> Int
+-- Takes a "Pasture" and returns the number of islands in it
+calculate :: Pasture -> Int
 calculate (w,h,gmap) = countIslands (findNonZero gmap) gmap
 
-countIslands :: [(Int,Int)]->Array (Int,Int) Int -> Int
+{- Provided a list of all locations that are non-zero(i.e. part of an island)
+ - and a 2d array of the pasture, this function does the following:
+   * takes the first non zero location, and performs a flood clear on it
+   * calls itself recursively with the updated map from the clear, and an
+     updated list of non-zero locations
+ - This method terminates when there are no more non-zero locations(or islands)
+ - left to check.
+ -}
+countIslands :: [(Int,Int)]->GrassMap-> Int
 countIslands [] _ = 0
 countIslands ((x,y):_) gmap = n + (countIslands (findNonZero newmap) newmap)
     where
         (newmap,n) = clearIsland x y gmap
 
--- Clears out an "island" starting at x,y
--- Returns 1 if cleared an island, otherwise 0
-clearIsland:: Int->Int->Array (Int,Int) Int -> (Array (Int,Int) Int, Int)
+{- Does a flood-clear starting at location x,y
+-- Returns 0 if there was nothing to clear(i.e. no island here)
+ - Returns 1 if it cleared something(i.e. there was an island here)
+ -
+ - Does the naive thing and clears the current element, then recursively calls
+ - itself on each neighboring element, passing the intermediate results along
+ -}
+clearIsland:: Int->Int->GrassMap-> (GrassMap, Int)
 clearIsland x y gmap | (x,y) `notElem` (indices gmap) = (gmap,0)
                      | gmap!(x,y)==0 = (gmap, 0)
                      | otherwise     = (ret,  1)
     where
+        -- The use of fst through here is just to get rid of the number, which
+        -- only matters to the calling function(and not to the recursive calls)
         ret = fst $ clearIsland   (x)   (y+1) $
                 fst $ clearIsland (x)   (y-1) $
                 fst $ clearIsland (x+1) (y+1) $
@@ -65,14 +87,15 @@ clearIsland x y gmap | (x,y) `notElem` (indices gmap) = (gmap,0)
                 fst $ clearIsland (x-1) (y)   $
                 fst $ clearIsland (x-1) (y-1) (gmap // (((x,y),0):[]))
 
-
-findNonZero :: Array (Int,Int) Int -> [(Int,Int)]
-findNonZero gmap =  x --trace (show x)
+{- This takes an array and returns a list of coordinates that contain non-zero
+ - values in them.  The list comprehension iterates over the entire list of
+ - possible indices, and comparing the value at those locations to zero.
+ -
+ -}
+findNonZero :: GrassMap-> [(Int,Int)]
+findNonZero gmap =  x
   where
     x = [ i | i<-(indices gmap), gmap!i /= 0]
-
--- -- printArray:: Array (Int,Int) Int -> String
--- printArray arr =
 
 -- Print the results
 display :: [Int] -> String
